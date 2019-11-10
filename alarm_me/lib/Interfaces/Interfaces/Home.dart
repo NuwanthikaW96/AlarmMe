b8992040a4c01b38e12f0ecffd469f9a7c31c0c7
yasith <ybuofficial97@gmail.com>
2019-11-10 13:51:47 +0530

    Update Home.dart

--- alarm_me/lib/Interfaces/Interfaces/Home.dart
+++ alarm_me/lib/Interfaces/Interfaces/Home.dart
@@ -1,10 +1,15 @@
-import 'package:alarm_me/Interfaces/Layout/AlarmHolder.dart';
 import 'package:alarm_me/Interfaces/Layout/BaseAppBar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 
 class Home extends StatefulWidget {
+  final FirebaseUser user;
+
+  Home({this.user});
+  
   @override
   _HomeState createState() {
     return _HomeState();
@@ -16,11 +21,22 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BaseAppBar(),
-      body: Column(
-        children: <Widget>[
-          AlarmHolder(),
-        ],
+      body: StreamBuilder(
+        stream: Firestore.instance
+        .collection('Alarms')
+        .where('UID', isEqualTo: widget.user.uid)
+        .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return new Text('Loading...');
+          return new ListView.builder(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) =>
+              _buildListItem(snapshot.data.documents[index]),
+          );
+        },
       ),
+      
+
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.alarm_add),
         onPressed: () {
@@ -31,6 +47,33 @@ class _HomeState extends State<Home> {
     );
   }
 }
+
+
+
+Widget _fireSearch(String queryText) {
+  return new StreamBuilder(
+    stream: Firestore.instance
+    .collection('Alarms')
+    .where('UID', isEqualTo: queryText)
+    .snapshots(),
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) return new Text('Loading...');
+      return new ListView.builder(
+        itemCount: snapshot.data.documents.length,
+        itemBuilder: (context, index) =>
+            _buildListItem(snapshot.data.documents[index]),
+      );
+    },
+  );
+}
+
+Widget _buildListItem(DocumentSnapshot document) {
+  return new ListTile(
+    title: document['title'],
+    subtitle: document['subtitle'],
+  );
+}
+
 
 // class MyHomePage extends StatefulWidget {
 //   @override
